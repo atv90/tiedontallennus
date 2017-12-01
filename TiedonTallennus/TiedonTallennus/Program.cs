405bd3f75639522208b152b9f7dda521fcfb3825
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2017-12-01 13:14:12 +0200

    Toteutettu lämpötilan asettaminen ja tallennus tiedostoon.

--- TiedonTallennus/TiedonTallennus/Program.cs
+++ TiedonTallennus/TiedonTallennus/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace TiedonTallennus
     {
         static void Main(string[] args)
         {
+            int lampotila = 0;
+            Console.Write("Anna uusi lämpötila: ");
+            String Syöte = Console.ReadLine();
+            lampotila = int.Parse(Syöte);
+
+            //tiedostoon kirjoittaminen
+
+            String tiedosto = @"C:\Users\Anne\Documents\OhjJatkoPaiva2Esimerkki\Lämpotila.txt";
+
+            File.WriteAllText(tiedosto, lampotila.ToString());
+            Console.WriteLine("Uusi lämpötila tallennettu.");
+
+            Console.ReadLine();
         }
     }
 }
