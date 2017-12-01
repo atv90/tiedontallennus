068a540cd1c28f4028b209b1fd80dfe82d12d251
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2017-12-01 13:33:03 +0200

    Lisätty aiemman lämpötilan lukeminen.

--- TiedonTallennus/TiedonTallennus/Program.cs
+++ TiedonTallennus/TiedonTallennus/Program.cs
@@ -11,14 +11,30 @@ namespace TiedonTallennus
     {
         static void Main(string[] args)
         {
+            String tiedosto = @"C:\Users\Anne\Documents\OhjJatkoPaiva2Esimerkki\Lämpotila.txt";
+
+            //luetaan tiedostosta aiemmin asetettu lämpötila
+            if (File.Exists(tiedosto))
+            {
+                string aiempiArvo = File.ReadAllText(tiedosto);
+                Console.WriteLine("Aiemmin asetettu lämpötila on " + aiempiArvo + " astetta.");
+            }
+            else
+            {
+                Console.WriteLine("Tervetuloa!");
+            }
+
             int lampotila = 0;
+
+            //WriteLine lisää automaattisesti rivinvaihdon rivin loppuun
+            //eli heittomerkkejä edeltävän merkin jälkeen
+            //Write ei lisää rivinvaihtoa vaan kohdistin jää sille paikalleen
             Console.Write("Anna uusi lämpötila: ");
             String Syöte = Console.ReadLine();
             lampotila = int.Parse(Syöte);
 
             //tiedostoon kirjoittaminen
 
-            String tiedosto = @"C:\Users\Anne\Documents\OhjJatkoPaiva2Esimerkki\Lämpotila.txt";
 
             File.WriteAllText(tiedosto, lampotila.ToString());
             Console.WriteLine("Uusi lämpötila tallennettu.");
